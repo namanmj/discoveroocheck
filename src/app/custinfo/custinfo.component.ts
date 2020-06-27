52990a09749420fa3fea59cc844f623d931f5204
namanmj <naman.18bcs1103@abes.ac.in>
2020-06-28 00:36:35 +0530

    second commit

--- src/app/custinfo/custinfo.component.ts
+++ src/app/custinfo/custinfo.component.ts
@@ -59,4 +59,20 @@ show(){
   this.showtodaydata=false;
   this.showalldata=true;
 }
+ openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("datadiv1").style.marginRight = "250px";
+  document.getElementById("datadiv2").style.marginRight = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("datadiv1").style.marginRight= "0";
+  document.getElementById("datadiv2").style.marginRight = "0px";
+  document.body.style.backgroundColor = "white";
+}
+logout(){
+  this.router.navigate(['']);
+}
 }
